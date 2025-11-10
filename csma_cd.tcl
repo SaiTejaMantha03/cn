19ed06b0e2ad5e9028eaf4ab6cc7df4884ed5bef
Mantha Sai Teja <151930706+SaiTejaMantha03@users.noreply.github.com>
2025-11-10 20:07:22 +0530

    Refactor CSMA/CD simulation setup and connections

--- csma_cd.tcl
+++ csma_cd.tcl
@@ -1,22 +1,13 @@
-# Set debug level
-LanRouter set debug_ 0
 
-# Create simulator instance
 set ns [new Simulator]
-
-# Define colors for NAM visualization
-$ns color 1 Green
-$ns color 2 Yellow
 
 # Open trace files
 set file1 [open out.tr w]
-set winfile [open WinFile w]
-$ns trace-all $file1
-
 set file2 [open out.nam w]
+$ns trace-all $file1
 $ns namtrace-all $file2
 
-# Define finish procedure
+# Finish procedure
 proc finish {} {
     global ns file1 file2
     $ns flush-trace
@@ -34,53 +25,35 @@ set n3 [$ns node]
 set n4 [$ns node]
 set n5 [$ns node]
 
-# Set node colors and shapes
-$n0 color red
-$n0 shape box
-$n1 color black
-$n1 shape box
-$n2 color blue
-$n3 color violet
-$n4 color orange
-$n4 shape box
-$n5 color brown
-$n5 shape box
-
 # Create duplex links
 $ns duplex-link $n0 $n2 2Mb 10ms DropTail
 $ns duplex-link $n1 $n2 2Mb 10ms DropTail
 $ns duplex-link $n2 $n3 2Mb 10ms DropTail
 
-# Create LAN with CSMA/CD protocol
+# Create LAN (CSMA/CD) for n3, n4, n5
 set lan [$ns newLan "$n3 $n4 $n5" 1Mb 40ms LL Queue/DropTail MAC/Csma/Cd Channel]
 
-# Setup TCP connection
+# TCP/FTP (n0 -> n4)
 set tcp [new Agent/TCP/Newreno]
 $ns attach-agent $n0 $tcp
 set sink [new Agent/TCPSink/DelAck]
 $ns attach-agent $n4 $sink
 $ns connect $tcp $sink
-$tcp set fid_ 1
 $tcp set window_ 8000
 $tcp set packetSize_ 1000
 
-# Setup FTP over TCP connection
 set ftp [new Application/FTP]
 $ftp attach-agent $tcp
-$ftp set type_ FTP
 
-# Setup UDP connection
+# UDP/CBR (n1 -> n5)
 set udp [new Agent/UDP]
 $ns attach-agent $n1 $udp
 set null [new Agent/Null]
 $ns attach-agent $n5 $null
 $ns connect $udp $null
-$udp set fid_ 2
 
-# Setup CBR over UDP connection
 set cbr [new Application/Traffic/CBR]
 $cbr attach-agent $udp
-$cbr set type_ CBR
 $cbr set packet_size_ 1110
 $cbr set rate_ 0.1mb
 $cbr set random_ false
